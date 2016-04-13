3bca3c13d071fc8048a699a8088d5c0bf0d99a95
Vedran Pavic <vedran.pavic@gmail.com>
2016-04-13 14:35:31 +0200

    Add modules

---
+++ spring-boot-actuator/src/main/java/hr/kapsch/javacro/actuator/ActuatorDemoApp.java
@@ -0,0 +1,13 @@
+package hr.kapsch.javacro.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorDemoApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorDemoApp.class, args);
+	}
+
+}
---
+++ spring-session/src/main/java/hr/kapsch/javacro/session/SessionDemoApp.java
@@ -0,0 +1,13 @@
+package hr.kapsch.javacro.session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionDemoApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionDemoApp.class, args);
+	}
+
+}
