829bd82e4929501807d261c8abc7e45133d2585a
Vedran Pavic <vedran.pavic@gmail.com>
2016-04-26 14:26:00 +0200

    Implement demo mbean

---
+++ src/main/java/hr/kapsch/javacro/actuator/JavaCroGreeterMbean.java
@@ -0,0 +1,21 @@
+package hr.kapsch.javacro.actuator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jmx.export.annotation.ManagedOperation;
+import org.springframework.jmx.export.annotation.ManagedResource;
+import org.springframework.stereotype.Component;
+
+@Component
+@ManagedResource
+public class JavaCroGreeterMbean {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(JavaCroGreeterMbean.class);
+
+	// hr.kapsch.javacro.actuator:name=javaCroGreeterMbean,type=JavaCroGreeterMbean/greet
+	@ManagedOperation
+	public void greet() {
+		LOGGER.info("Hello JavaCRO '16!");
+	}
+
+}
